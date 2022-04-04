eb67d6c0d621a81b9f199c179a405da19f070947
Caroline <carolinesitter24@gmail.com>
2022-04-04 14:28:57 -0500

    Initial Commit - Finished Functs 1-4)

---
+++ trials.py
@@ -0,0 +1,64 @@
+"""Python functions for JavaScript Trials 1."""
+
+
+def output_all_items(items):
+    # #pass  # create for loop, iterate through items
+    for item in items:
+        print(item)
+
+
+def get_all_evens(nums):
+    #pass  # create empty list and for loop for even numbers
+    even_numbers = []
+
+    for num in nums:
+        if num % 2 == 0:
+            even_numbers.append(num)
+    return even_numbers
+
+
+def get_odd_indices(items):
+    #pass  # iterate through items and return odd index
+    result = []
+    for idx in range(len(items)):
+        if idx % 2 != 0:
+            result.append(items[idx])
+        
+    return result
+
+
+def print_as_numbered_list(items):
+    #pass  # create a numbered list of items
+    i = 1
+
+    for item in items:
+        print(f"{i}. {item}")
+        i += 1
+
+
+def get_range(start, stop):
+    pass  # TODO: replace this line with your code
+
+
+def censor_vowels(word):
+    pass  # TODO: replace this line with your code
+
+
+def snake_to_camel(string):
+    pass  # TODO: replace this line with your code
+
+
+def longest_word_length(words):
+    pass  # TODO: replace this line with your code
+
+
+def truncate(string):
+    pass  # TODO: replace this line with your code
+
+
+def has_balanced_parens(string):
+    pass  # TODO: replace this line with your code
+
+
+def compress(string):
+    pass  # TODO: replace this line with your code
